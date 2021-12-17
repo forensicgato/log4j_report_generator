9d58becaa152865eab07dd8d3e24ee3f0e9f6028
Leon <24562461+forensicgato@users.noreply.github.com>
2021-12-17 18:05:20 +0100

    updated filter to use correct results instead of emoji

--- log4j_generateReport.ps1
+++ log4j_generateReport.ps1
@@ -15,19 +15,29 @@ $logs | ForEach-Object {
     $lines = Get-Content $_.FullName
     $serverName = $_.Basename
     $lines | ForEach-Object {
-        if ($_.Contains(":-(")) {
+        if ($_.Contains("_VULNERABLE_")) {
             Write-Host "Vulnerable .jar" + $serverName
             $logInfo = $serverName + ";" + "VULN" + ";" + "$_" + ";"
             Add-Content $csv $logInfo
         }
-        elseif ($_.Contains(":-|")) {
+        elseif ($_.Contains("_OLD_")) {
             Write-Host "Check .jar" + $serverName
             $logInfo = $serverName + ";" + "OLD" + ";" + "$_" + ";"
             Add-Content $csv $logInfo
         }
-        elseif ($_.Contains(":-)")) {
-            Write-Host "SAFE .jar" + $serverName
+        elseif ($_.Contains("_SAFE_")) {
+            Write-Host "safe .jar" + $serverName
             $logInfo = $serverName + ";" + "SAFE" + ";" + "$_" + ";"
+            Add-Content $csv $logInfo
+        }
+        elseif ($_.Contains("_OKAY_")) {
+            Write-Host "okay .jar" + $serverName
+            $logInfo = $serverName + ";" + "OKAY" + ";" + "$_" + ";"
+            Add-Content $csv $logInfo
+        }
+        elseif ($_.Contains("_POTENTIALLY_SAFE_")) {
+            Write-Host "potentially safe .jar" + $serverName
+            $logInfo = $serverName + ";" + "PSAFE" + ";" + "$_" + ";"
             Add-Content $csv $logInfo
         }
     }
